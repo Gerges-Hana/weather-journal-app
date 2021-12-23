fb27ed3349bab2f5ffc99b8cb6dceca8124da74c
Gerges-Hana <83248914+Gerges-Hana@users.noreply.github.com>
2021-12-23 12:21:12 +0200

    update in update function

--- server.js
+++ server.js
@@ -32,27 +32,19 @@ app.use(express.static('website'));
 //when server running this function is called
 app.listen( port ,() =>{
     console.log("Hi gerges");
-<<<<<<< HEAD
     console.log(`server Running : http://Localhost:${port}`);
-=======
-    console.log(`server Running : https://Localhost:${port}`);
->>>>>>> 00c4850b84d559c7bfa5e01796aa13541cf32cc0
     }
 );
 // get function to have data from server 
 // get route to server as server return the data needed 
 app.get('/getAll', (request , response) =>{
-<<<<<<< HEAD
 response.send(projectData).end();
-=======
-response.send(projectData);
->>>>>>> 00c4850b84d559c7bfa5e01796aa13541cf32cc0
 });
 
 // post function to post of projectData that have {temp ,date, content }
 //  route to post the data from client to server  
 app.post('/postData',(request, response) =>{
-<<<<<<< HEAD
+
     projectData={
         temp:request.body.temp,
         date:request.body.date,
@@ -61,14 +53,5 @@ app.post('/postData',(request, response) =>{
     response.send(projectData).end();
     // projectData={...request.body};
     // response .end();
-=======
-    // projectData={
-        // temp:request.body.temp,
-        // date:request.body.date,
-        // content:request.body.content
-    // };
-    // response.send(projectData);
-    projectData={...request.body};
-    response .end();
->>>>>>> 00c4850b84d559c7bfa5e01796aa13541cf32cc0
+
 });
--- website/app.js
+++ website/app.js
@@ -8,14 +8,9 @@ const content =document.getElementById('content');
 const zipNum = Number(zipCode.value);
 const zipValue = zipCode.value;
 const ApiKey="2342ff72b9427aac95efdc9dcf185461";
-<<<<<<< HEAD
 const Appurl="http://Localhost:1111/";
-// const fulURL =`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode.value}&appid=${ApiKey}&units=metric`;
-// const ApiKey_all=`&appid=2342ff72b9427aac95efdc9dcf185461&units=metric`;
-=======
 const fulURL =`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode.value}&appid=${ApiKey}&units=metric`;
 const ApiKey_all=`&appid=2342ff72b9427aac95efdc9dcf185461&units=metric`;
->>>>>>> 00c4850b84d559c7bfa5e01796aa13541cf32cc0
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
@@ -26,45 +21,34 @@ let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 // then validated if when you dont have zip code as alert msagon browser 
 // that hav all function 
 generateBtn.addEventListener('click',function(){ 
-<<<<<<< HEAD
 
-=======
-const fulURL =`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode.value}&appid=${ApiKey}&units=metric`;
-// console.log(fulURL);
->>>>>>> 00c4850b84d559c7bfa5e01796aa13541cf32cc0
     const Data ={
         zip_data:zipCode.value,
         content_data:feelings.value,
         date_datd: newDate
 };
+
 if(zipCode.value.length<=0){
     alert("zip code is required");
 }
-<<<<<<< HEAD
-    // console.log(Data);
+
     getData(Data.zip_data);
     postData(Data);
-    // updateUi();
-=======
-    console.log(Data);
-    getData();
-    postData(Data.temp,Data.content_data);
-    updateUi();
->>>>>>> 00c4850b84d559c7bfa5e01796aa13541cf32cc0
     
+
 });
 
 // ////////////////
 // get function to return temp fron api server 
 // if zip cod find as thes return temp else veiow alert not fond temp of thes zipcode 
-<<<<<<< HEAD
+
 const getData = async function(zip_data){
     const request = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zip_data}&appid=${ApiKey}&units=metric`);
     try{
         const response = await request.json();
         // console.log(response);
         const temp = response.main.temp;
-        // console.log(temp);
+        return temp;
     }
     catch(err){
         alert(err);
@@ -82,14 +66,17 @@ const postData = async function(Data){
       body:JSON.stringify(Data) 
   
     });
+    
+//    console.log(Data);
    
   try{
       response.json().then(Data =>{
     if(response.ok){
-        console.log("alla7 ynawer");
+        console.log("the process successfully completed");
+        updateUi();
     }
     else{
-        alert("falid your post data")
+        alert("Process not completed")
     }
 });
 }
@@ -104,69 +91,21 @@ const postData = async function(Data){
 /////////////////////////////////
 // update function to get datd from local server and push it in html badge 
 // select evre elemnt in thml and set the value matched it in  server 
-// const updateUi = async function(){
-//     const request = await fetch(`/getAll`);
-//     try{
-//         const response = await request.json();
-//         date.innerHTML=`Date : ${response.date}`;
-//         temp.innerHTML=`Temperature :${ response.temp}`;
-//         content.innerHTML=`Feel : ${response.content}`;
-//         console.log(response);
-//         return response;
-//     }catch(err){
-//         console.log(err);
-//     }
-// };
-=======
-const getData = async function(){
-    const request = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=10001&appid=${ApiKey}&units=metric`);
-    
-    try{
-        const response = await request.json();
-        
-        const temp = response.main.temp;
-        return temp;
-    }
-    catch(err){
-        console.log(err);
-    }
-};
-// // ///////////////////////////////////
-// post function that have temp info to post it to server
-// post my data have to server 
-const postData = async function(temp,feelings){
-    const request = await fetch(`/postData`
-    ,{
-      method:'POST',
-      headers:{
-          'Content-Type':'application/json',
-      },
-      body:JSON.stringify({
-          date:newDate,
-          temp:temp,
-          feelings:feelings
-      }) 
-  
-    });
- 
-};
-
-/////////////////////////////////
-// update function to get datd from local server and push it in html badge 
-// select evre elemnt in thml and set the value matched it in  server 
 const updateUi = async function(){
-    const request = await fetch(`/getAll`);
+  const response = await fetch(`${Appurl}getAll`);
     try{
-        const response = await request.json();
-        date.innerHTML=`Date : ${response.date}`;
-        temp.innerHTML=`Temperature :${ response.temp}`;
-        content.innerHTML=`Feel : ${response.content}`;
+        response.json().then(Data=>{
+
+        date.innerHTML=`Date : ${Data.date}`;
+        temp.innerHTML=`Temperature :${ Data.temp}`;
+        content.innerHTML=`Feel : ${Data.content}`;
+        console.log(Data);
         console.log(response);
-        return response;
+         });
     }catch(err){
         console.log(err);
     }
 };
->>>>>>> 00c4850b84d559c7bfa5e01796aa13541cf32cc0
 
-// ///////////////////////////////////
+
+/////////////////////////////////
